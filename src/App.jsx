1ca28c3e13b071915ce584d9415075adc1e81f7b
Mateo Enrico <mateoenrico1221@gmail.com>
2023-11-13 22:30:04 -0300

    arreglo errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Home from './components/Home/Home';
-import Products from './components/Products/Products';
-import Financing from './components/Financing/Financing';
-import Brands from './components/Brands/Brands';
-import Sponsors from './components/Sponsors/Sponsors';
+import NavBar from './components/NavBar/NavBar.jsx';
+import Home from './components/Home/Home.jsx';
+import Products from './components/Products/Products.jsx';
+import Financing from './components/Financing/Financing.jsx';
+import Brands from './components/Brands/Brands.jsx';
+import Sponsors from './components/Sponsors/Sponsors.jsx';
 
 const App = () => {
   return (
