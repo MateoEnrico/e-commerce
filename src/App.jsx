e0cb43907d1a339fecf6eef71c09d0d2f02678f1
Mateo Enrico <mateoenrico1221@gmail.com>
2023-11-13 23:29:08 -0300

    ultimo deplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Home from './components/Home/Home';
-import Products from './components/Products/Products';
-import Financing from './components/Financing/Financing';
-import Brands from './components/Brands/Brands';
-import Sponsors from './components/Sponsors/Sponsors.';
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
+import Products from './Components/Products/Products';
+import Financing from './Components/Financing/Financing';
+import Brands from './Components/Brands/Brands';
+import Sponsors from './Components/Sponsors/Sponsors';
 
 const App = () => {
   return (
